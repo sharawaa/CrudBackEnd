b4aad9dd2c9f670921fee5707b21fca7f2f5975f
leap2022 <sharawnyambuuurtnasan@gmail.com>
2023-03-13 08:20:44 +0800

    asda

--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,21 @@
 import express from "express";
 const Router = express.Router();
-import { getProducts } from "../services/prod-service.js";
+import { addProduct, getBrands, getProducts } from "../services/prod-service.js";
 
 Router.get("/products", async (req, res) => {
   const result = await getProducts();
   console.log(result);
   res.status(200).json(result);
 });
+Router.get("/brands", async (req, res)=>{
+  const progress = await getBrands();
+  console.log(progress);
+  res.status(200).json(progress)
+})
 export default Router;
+Router.post("/product", async(req, res)=>{
+  const result = await addProduct();
+  console.log(req.body)
+  console.log(result)
+  res.status(200).json(result)
+})
--- services/prod-service.js
+++ services/prod-service.js
@@ -1,7 +1,15 @@
 import { pool } from "../config/mysql-config.js";
 export async function getProducts() {
   const [rows] = await pool.query(
-    `SELECT * FROM brands INNER JOIN products ON brands.id = products.brand_id INNER JOIN category ON products.category_id = category.id;`
+    `SELECT products.id, products.image, products.name, products.price, products.sale, brands.name as brand, category.name as category FROM brands INNER JOIN products ON brands.id = products.brand_id INNER JOIN category ON products.category_id = category.id;`
   );
+  return rows;
+} export async function getBrands(brand){
+  const [ruws] = await pool.query(`SELECT * FROM products WHERE brand_id in (SELECT id FROM brands WHERE name= '${brand}');`);
+  return ruws;
+}
+
+export async function addProduct(){
+  const [rows] = await pool.query(`INSERT INTO products(image, name, price, brand_id, category_id, sale) VALUES( 'zurag', 'gar utas', 200, 1005, 2001, 20);`)
   return rows;
 }
